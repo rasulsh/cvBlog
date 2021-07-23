49ab0e602dc7b42a06ffbb3e8d358d920805ed18
rasulsh <me@rasulsh.ir>
2021-07-23 04:54:38 +0430

    first commit

---
+++ resources/lang/fa/common.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the index blade file
+    |
+    */
+
+    'heading_title'			=> 'مهندس نرم افزار و توسعه دهنده اپن کارت',
+    'developer_name'		=> 'رسول شاهپوری',
+    'contact_me'			=> 'تماس با من',
+    'home'					=> 'خانه',
+    'about_me'				=> 'درباره من',
+    'skills'				=> 'مهارت ها',
+    'services'				=> 'خدمات',
+    'show_services'			=> 'مشاهده خدمات',
+    'mobile'				=> 'موبایل',
+    'email'					=> 'ایمیل',
+    'address'				=> 'آدرس',
+
+];
---
+++ resources/lang/fa/social_link.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the index blade file
+    |
+    */
+
+    'instagram'	=> '',
+    'telegram'	=> '',
+    'whatsapp'	=> '',
+    'facebook'	=> '',
+    'linkedin'	=> '',
+    'twitter'	=> '',
+    'github'	=> 'https://github.com/rasulsh',
+    'email'		=> 'me@rasulsh.ir',
+    'address'	=> 'تهران - ستارخان',
+
+];
